2a1dbe0871413ff88cfba77b25b23eb7161ad3bf
Your Name <you@example.com>
2019-11-17 19:46:21 +0900

    first commit

---
+++ src/test/java/com/example/springRestWebService/SpringRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRestWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
